7605e45c1facc9ec7067e689ee69289c38eaa301
Jasur <j31548359@gmail.com>
2025-07-29 14:04:37 +0500

    url

--- client/src/context/services/api.js
+++ client/src/context/services/api.js
@@ -1,8 +1,9 @@
 import { createApi, fetchBaseQuery, retry } from "@reduxjs/toolkit/query/react";
 
 const rawBaseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:8080/api/v1",
+  // baseUrl: "http://localhost:8080/api/v1",
   // baseUrl: "https://kwmkqg1t-8080.euw.devtunnels.ms/api/v1",
+  baseUrl: "https://tilla-zavod-server.vercel.app/api/v1",
   prepareHeaders: (headers) => {
     const token = localStorage.getItem("token");
     if (token) {
--- client/src/socket.js
+++ client/src/socket.js
@@ -2,7 +2,8 @@ import io from "socket.io-client";
 
 // const SOCKET_URL = `https://kwmkqg1t-8080.euw.devtunnels.ms`;
 // const SOCKET_URL = `https://idish25.richman.uz`;
-const SOCKET_URL = `http://localhost:8080`;
+// const SOCKET_URL = `http://localhost:8080`;
+const SOCKET_URL = `https://tilla-zavod-server.vercel.app`;
 const socket = io(SOCKET_URL, { transports: ["websocket"] });
 
 export default socket;
